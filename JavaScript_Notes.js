de7689c699e0b88edc26cabc80417176edd8f27d
perfectsquare123 <91461666+perfectsquare123@users.noreply.github.com>
2022-10-21 21:16:29 +0800

    Update JavaScript_Notes.js

--- JavaScript_Notes.js
+++ JavaScript_Notes.js
@@ -147,7 +147,7 @@ var ourDog = {
 // dot notation
 var nameValue = ourDog.name;    // Camper
 
-// Bracket notation  -- required to use if properties name has spaces
+// Bracket notation  -- required to use if properties name has spaces " "
 var friends = ourDog["dog friends"];    // [ 'everything!' ]   // can use " " or ' '
 var f = 16;
 var FF = ourDog[f];         // Nanth
@@ -155,3 +155,267 @@ var FF = ourDog[f];         // Nanth
 // Updating Object Properties
 ourDog.legs = 5;       // "legs": 5
 
+// Add property to object 
+// 2 ways to add
+ourDog.bark = "bow-wow";
+ourDog["smell"] = 123;
+
+// Delete property 
+// use the keyWord 'delete'
+delete ourDog.name;
+// console.log(ourDog);
+
+// use it as DICTIONARY
+// replacing switch statement
+// [switch statement]
+var result = "";
+var val = "beta";
+
+switch(val){
+    case "alpha": 
+        result = "adams";
+        break;
+    case "beta" : 
+        result = "bettrice";
+        break;
+    // ... and so on
+}
+console.log(result);   // bettrice
+
+// now change to =>
+// [property: value]
+var lookup = {
+    "alpha": "adams",
+    "beta": "bettrice",
+};
+
+console.log(lookup[val]);   // bettrice
+
+// The `hasOwnProperty()` method returns a boolean 
+// indicating whether the object has the specified property as its own property (as opposed to inheriting it).
+function checkObj(checkProp){
+    if(lookup.hasOwnProperty(checkProp)) return lookup[checkProp];
+    else return "not found!";
+}
+console.log(checkObj("alpha"));     // adams
+console.log(checkObj("ALPHA"));     // not found!
+
+// manipulating complex object
+// var = [ {..object1..}, {..object2..}] -- objects inside array
+var myMusic = [
+    //object1
+    {
+        "artist": "YuXin",
+        "title": "baby i know",
+        "release_year": 2022,
+        "formats": ["CD", "8T", "LP"],
+        "gold": true
+    },
+    // object2
+    {
+        "artist": "XiaoZhan",
+        "title": "qingYuNian",
+    }
+];
+
+// nested object
+var myStorage = {
+    "car": {
+        "inside":{
+            "glove box": "maps",
+            "plane seats =)": "lab"
+        },
+        "outside":{
+            "metal": "red"
+        }
+    }
+};
+
+var context = myStorage.car.inside["plane seats =)"];
+console.log(context);
+
+// nested array
+var myPlant = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulips",
+            "sunflower",
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+        ]
+    }
+];
+
+console.log(myPlant[1].list[1]);    // pine
+
+// Record Collection
+var collection = {
+    "1234": {
+        album: "whatever",
+        artist: "someone famous",
+        tracks: [
+            "music1",
+            "music2"
+        ]
+    },
+    "5678": {
+        album: "friend",
+        artist: "someone not famous",
+        tracks: [
+            "music3",
+            "music4"
+        ]
+    },
+}
+
+// keep a copy of the collection for tests
+// A common use of JSON is to exchange data to/from a web server.
+// When receiving data from a web server, the data is always a string.
+// Parse the data with JSON.parse(), and the data becomes a JavaScript object.
+// When sending data to a web server, the data has to be a string.
+// Convert a JavaScript object into a string with JSON.stringify().
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id, prop, value){
+    if (value === ""){
+        delete collection[id][prop];
+    }
+    else if(prop === "tracks"){
+        collection[id][prop] = collection[id][prop] || [];      // if this exist -- set it to itself; if not exist -- set it into empty array
+        collection[id][prop].push(value);
+    }
+    else {
+        collection[id][prop] = value;
+    }
+    return collection;
+}
+
+updateRecords(5678, "tracks", "test")       // push in new elements into track array
+console.log(updateRecords(1234, "year", "2001"));   // since this property is not exist before that, so it will add into
+
+// while loop
+// for loop
+// can directly access the length of array by -- arr[i].length => find the length of nested array[i]
+// do while loop
+
+var contact = [
+    {
+        firstName: "Sher",
+        lastName: "lock",
+        age: 35,
+        likes: ["java", "play"],
+    },
+    {
+        firstName: "manys",
+        lastName: "popois",
+        age: 3,
+        likes: ["javaScript", "playing"],
+    },
+]
+
+function lookupProfile( name, prop){
+    for(var i=0; i< contact.length; i++){
+        if(contact[i].firstName === name){
+            // fance function to check whether this property exist
+            return contact[i][prop] || "no such property";
+        }
+    }
+    return "no such contact";
+}
+
+console.log(lookupProfile("Sher", "age"));      //35
+
+// generate random number
+// Math.random()  -- generate [0,1) ie not include 1
+// [decimal]
+number = Math.random();
+// Math.floor()  -- round down
+// [integer] 
+randomNumberBetween0and19 = Math.floor(Math.random()*20);       // 0-19
+// [Random range]
+min = 5; max = 15
+randomRange = Math.floor(Math.random() * (max-min+1)) + min;    // 5-15
+//console.log(randomRange);
+
+// NaN -- Not a Number
+function convertToInteger(str){
+    //return parseInt(str);
+    return parseInt(str, 2)     // second parameter -- radix 
+    // if radix = 2, ie str is in binary form and will be converted into base 10 int
+}
+
+console.log(convertToInteger("10010"));
+
+// Ternary Operator
+// Format:
+// condition ? statement-if-true : statement-if-false
+check = (a===b) ? true : false;     // false
+
+// multiple Ternary Operator
+// including else-if
+var check;
+var num;
+check = num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+
+// variable declaration [ var, let , const ]
+
+// var vs let
+// `let` -- declares limited to a block-scoped local variable, or expression on which it is used, optionally initializing it to a value.
+//       -- initialized to a value only when a parser evaluates it
+//       -- begins declarations, not statements
+//       -- so that it will only appear within the block/scope that we want them to appear
+//       -- allow to declare once only, if (let var = certain number) happens twice => error
+// `var` -- declares a variable globally, or locally to an entire function regardless of block scope
+//       -- no error given while declared twice
+//       -- rarely use for declaration
+
+// "use strict";
+// -- enable strict mode which catches common coding mistakes and unsafe actions
+
+function checkScope(){
+    "use strict";
+    //var i = "function scope";
+    if(true){
+        var i = "block scope";
+        console.log("Block scope i is : ", i);
+        // let, let -- block; var, var -- block; // , let -- block; // , var -- block
+    }
+    console.log("Function scope i is : ", i);
+    // let, let -- function; var, var -- block; // , let -- ReferenceError: i is not defined; // , var -- block
+}
+checkScope();
+
+// const
+// -- have all the property of `let` but cannot reassign a const
+// -- usually give the name UPPER CASE
+// *** most of the time use `let` and `const` only, rarely use `var`
+
+// Mutate an array declared with const
+const s = [5, 2, 7]
+function editInPlace(){
+    "use strict";
+
+    // s = [2,5,7]    -- ERROR
+    // but the following works
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+
+    return s;
+}
+
+console.log(editInPlace());
+
+// prevent object mutation
+// `Object.freeze()'
+const MATH_CONSTANTS = {
+    PI: 3.142
+};
+Object.freeze(MATH_CONSTANTS);
